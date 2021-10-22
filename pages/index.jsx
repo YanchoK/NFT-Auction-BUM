1f6bfdd7dd453d747c5d4a9c989120751b9139d1
Yancho Kostov <yankostov1@gmail.com>
2021-10-22 12:25:16 +0300

    feet: add avatar

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,8 @@
 import Example from "../src/components/example/Example";
 import Logo from "../src/components/logo/Logo"
 import Header from "../src/components/header/Header"
+import Avatar from "../src/components/avatar/Avatar"
 
 export default function Index() {
-  return <Header />;
+  return <Avatar url="D:\1. Programming\Boom.dev\Projects\nft-auction-bum\public\images\avatar.png" />;
 }
