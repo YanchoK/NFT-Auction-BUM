6d8eeecb22f4cec13375eab1d9017c363b972c92
Yancho Kostov <yankostov1@gmail.com>
2021-11-18 15:08:50 +0200

    feat: add footer

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,12 +8,10 @@ import Trending from "../src/components/trending/Trending";
 import Auctions from "../src/components/auctions/Auctions";
 import dataNfts from "../data/nfts.json";
 import { useState , useEffect } from "react";
+import Footer from "../src/components/footer/Footer";
 
 
 export default function Index() {
-
-
-  return <Auctions cards={[{"name":"Clock","user":{"avatarUrl":"images/avatar.png","verified":true},"mediaUrl":"images/nft.jpg","price":200,"currency":"BTC","timeLeft":3600000},{"name":"DOGE","user":{"avatarUrl":"images/avatar.png","verified":true},"mediaUrl":"images/nft.jpg","price":200,"currency":"BTC","timeLeft":3600000},{"name":"BTC","user":{"avatarUrl":"images/avatar.png","verified":true},"mediaUrl":"images/nft.jpg","price":100,"currency":"BTC","timeLeft":3600000},{"name":"Litecoin","user":{"avatarUrl":"images/avatar.png","verified":true},"mediaUrl":"images/nft.jpg","price":300,"currency":"BTC","timeLeft":3600000}]} />
-  
+  return <Footer />
 }
 
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,26 @@
+import Container from "@mui/material/Container"; 
+import Grid from '@mui/material/Grid';   
+import Logo from "../logo/Logo";
+import styles from "./Footer.module.scss";
+import Button from "@mui/material/Button";
+
+export default function Footer() {
+    return(
+        <div className={styles.wrapper}>
+            <Container className={styles.container} maxWidth="xl">
+                <Grid container direction='row' columnSpacing={10} >
+                    <Grid item >
+                        <Logo type='muted' />
+                    </Grid>
+                    <Grid item className={styles.text}>
+                        <p>Bum All Right Reserved 2021</p>
+                    </Grid>
+                    <Grid item xs sx={{ direction : 'rtl' }}>
+                        <Button sx={{ color: 'white' , borderRadius: 10 }}>Privacy Policy</Button>
+                        <Button sx={{ color: 'white' , borderRadius: 10 }} >Cookie Policy</Button>
+                    </Grid>
+                </Grid>
+            </Container>
+        </div>
+    );
+}
